3310c191f7bd65827d7ea3cb7293efe07c6774a3
Victor Quan <victorquan2010@gmail.com>
2022-09-01 19:06:55 -0400

    updated website to include various features

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
@@ -19,13 +20,36 @@ connectDB();
 
 const app = express();
 
+// body parser middleware
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// method override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  }))
+
 // logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+// handlebars helpers
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
+
 // handlebars
-app.engine('.hbs', engine({extname: '.hbs'}));
+app.engine('.hbs', engine({helpers: {
+    formatDate,
+    stripTags,
+    truncate,
+    editIcon,
+    select,
+}, extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
@@ -41,12 +65,19 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+// set global var
+app.use(function (req,res,next){
+    res.locals.user = req.user || null;
+    next();
+})
+
 // static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
 app.use('/', require('./routes/index'));
-app.use('/auth', require('./routes/auth'))
+app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
  
 const PORT = process.env.PORT || 8001;
 
